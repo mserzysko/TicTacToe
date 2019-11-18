79068975fc95e6c1ff06f8b6719a57178014b6e6
mserzysko <mserzysko24@gmail.com>
2019-11-18 10:53:07 +0100

    game works, but it allow for overwriting

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,18 +1,5 @@
-
-
-
 boardvalues = [' ']*10
 wins=[(1,2,3), (4,5,6), (7,8,9), (1,5,9), (3,5,7), (1,4,7), (2,5,8), (3,6,9)]
-
-
-def board(boardvalues):
-    ''' Prints our board '''
-    print(boardvalues[1]+'|'+boardvalues[2]+'|'+boardvalues[3])
-    print('-'+ '|' + '-' + '|' + '-')
-    print(boardvalues[4]+'|'+boardvalues[5]+'|'+boardvalues[6])
-    print('-' + '|' + '-' + '|' + '-')
-    print(boardvalues[7]+'|'+boardvalues[8]+'|'+boardvalues[9])
-
 
 print('Welcome to Tic Tac Toe!')
 print('Our board looks like this:')
@@ -26,6 +13,13 @@ print('7'+'|'+'8'+'|'+'9')
 print('Insert number representing a cell in order to choose that cell')
 print("Ready? Let's play!")
 
+def board(boardvalues):
+    ''' Prints our board '''
+    print(boardvalues[1]+'|'+boardvalues[2]+'|'+boardvalues[3])
+    print('-'+ '|' + '-' + '|' + '-')
+    print(boardvalues[4]+'|'+boardvalues[5]+'|'+boardvalues[6])
+    print('-' + '|' + '-' + '|' + '-')
+    print(boardvalues[7]+'|'+boardvalues[8]+'|'+boardvalues[9])
 
 def player_marker():
     '''Should choose markers for the players'''
@@ -46,15 +40,11 @@ def player_marker():
 
     print(f'Player1 is {player1}, player2 is {player2}')
 
-
 player_marker()
 print('Player1: start!')
 
-
 def check_if_win():
     global winner
-    #mam wrazenie ze cos jest nie tak z tym for a, b, c in wins. albo moze podrukowac co jest w tych pal
-
     if len(pla1)>2:
         for a, b, c in wins:
             if a in pla1 and b in pla1 and c in pla1:
@@ -70,36 +60,40 @@ def check_if_win():
 
 
 
-
-
-
-
 pla1=[]
 pla2=[]
 winner=''
 player=True
 while not winner:
-    check_if_win()
 
     if player:
         ind=int(input('Player1: '))
+
+        #
+
+        if pla1!=[]:
+            if ind in pla1 or ind in pla2:
+                print('Place occupied! Try again!')
+                break
+            else:
+                continue
+
         boardvalues[ind]=player1
         pla1.append(ind)
         player=not player
-
         board(boardvalues)
-
         check_if_win()
-# if check_if_win nie ma po tych if-ach to się zawiesza z jakiegoś powodu
     else:
-        ind = int(input('Player2: '))
+        ind=int(input('Player2: '))
+
+
+
         boardvalues[ind] = player2
         pla2.append(ind)
         player = not player
-
         board(boardvalues)
         check_if_win()
 
 
-# Działa! Ale nie działa sprawdzanie czy jest wygrana
+
 # można nadpisywać numerki: trzeba to ukrócić
